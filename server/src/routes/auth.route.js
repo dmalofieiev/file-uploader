551a7a6e753cd16bbdba4550b3f839c557f860b2
Dmytro Malofieiev <maloy34522@gmail.com>
2023-05-10 13:00:40 +0200

    added login functional

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -3,19 +3,38 @@ const bcrypt = require("bcrypt");
 const { User } = require("../../db/models");
 
 router.post("/register", async (req, res) => {
-  console.log("$$$$$$$$$$$$$");
   const { user_name, email, password } = req.body;
   const hashPass = await bcrypt.hash(password, 10);
   const newUser = await User.findOrCreate({
     where: { email },
     defaults: { user_name, password: hashPass },
   });
-  console.log("^^^^^^^newUser^^^^^^^", newUser);
   if (newUser[1]) {
     req.session.user = newUser[0];
+    console.log("req.session: -------->", req.session);
     res.json({ msg: "Пользователь зарегистрирован" });
   } else {
     res.json({ msg: "Пользователь уже существует" });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({
+    where: { email },
+    plain: true,
+  });
+  if (user) {
+    const passCheck = await bcrypt.compare(password, user.password);
+    if (passCheck) {
+      req.session.user = user;
+      console.log(req.session);
+      res.json({ msg: "Удача!" });
+    } else {
+      res.json({ msg: "Неверный пароль!" });
+    }
+  } else {
+    res.json({ msg: "Такого юзера не существует" });
   }
 });
 
