6f90bdb9c5f6a2d77c8e3c3255d23ebdeee1f301
George <gslansky@yandex.ru>
2023-05-10 12:10:25 +0300

    auth-state

--- client/src/components/Authentification/Register/Register-modal.tsx
+++ client/src/components/Authentification/Register/Register-modal.tsx
@@ -1,30 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { showModal, hideModal } from "../../../redux/slicers/Auth.slicer";
+import { RootState } from "../../../redux/store";
+
+import { styled } from '@mui/system';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { FormControl, FormLabel } from '@mui/material';
+
+type Input = {
+  user_name: string,
+  email:string,
+  password:string,
+}
+
+const initialState: Input = {
+  user_name: '',
+  email: '',
+  password:''
+}
 
 function Register() {
   const dispatch = useDispatch();
+  const state = useSelector((state: RootState) => state.authSlice.open)
+
+  const [inputs, setInputs] = useState(initialState)
+
+  const changeHandler = (e: any) => {
+    console.log('test 123')
+    setInputs((prev)=> ({...prev, [e.target.name]: e.target.value}))
+  }
+  console.log(inputs);
+  
 
   return (
     <div>
-      <Button onClick={dispatch(showModal)}>Open modal</Button>
       <Modal
-        open={}
-        onClose={handleClose}
+        open={state}
+        onClose={()=> dispatch(hideModal())}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+        <Box>
+          <FormControl>
+            <FormLabel >Enter Name</FormLabel>
+            <TextField onChange={changeHandler } name="user_name" type="text"></TextField>
+            <FormLabel>Enter Email</FormLabel>
+            <TextField onChange={changeHandler }  name="email" type="email"></TextField>
+            <FormLabel>Enter Password</FormLabel>
+            <TextField onChange={changeHandler }  name="password" type="password"></TextField>
+            <Button>Submit</Button>
+          </FormControl>
         </Box>
       </Modal>
     </div>
--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { showModal, hideModal } from "../../redux/slicers/Auth.slicer";
 import { useDispatch } from "react-redux";
+import Register from "../Authentification/Register/Register-modal";
 
 export default function Main() {
   const dispatch = useDispatch();
   return (
     <div>
       <h1>Загружатор</h1>
-      <button onClick={}>Логин</button>
-      <button>Регистрация</button>
+      <button>Логин</button>
+      <button onClick={()=> dispatch(showModal())}>Регистрация</button>
+      <Register />
     </div>
   );
 }
